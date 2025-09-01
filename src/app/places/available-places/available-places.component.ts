c1d80597bc80203f3f0bcea2c35c87dbcf52defc
robertgutu <gutu.robert49@yahoo.com>
2025-09-01 16:33:25 +0300

    get request + store data in a signal

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -12,7 +12,25 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef)
+
+  ngOnInit(): void {
+      const subs = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe(
+        {
+          next: (resData) => {
+            console.log('resData:',resData.places);
+            this.places.set(resData.places)
+          }
+        }
+      )
+
+      this.destroyRef.onDestroy( () => {   
+          subs.unsubscribe();
+        } 
+      )
+  }; 
+
 }
