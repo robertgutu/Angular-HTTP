09464e8e97ce8d4eebddd3c711f6c289a0da4f52
robertgutu <gutu.robert49@yahoo.com>
2025-09-02 11:46:53 +0300

    more data fetched

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { map,catchError,throwError } from 'rxjs';
+
+const apiUrl = 'http://localhost:3000'
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +15,41 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit{
+  userPlaces = signal<Place[]>([])
+  isFetchingData = signal(false)
+  error = signal('')
+
+  private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef)
+
+  ngOnInit(): void {
+    this.isFetchingData.set(true)
+    const url = `${apiUrl}/user-places`
+    const subscribbe = this.httpClient.get<{ places: Place[] }>(url)
+    .pipe(
+      map((resData) => resData.places),
+      catchError((error) => {
+        console.log('err:',error)
+        return throwError( () => {
+          return new Error("Something went wrong while fetching user data")
+        })
+      })
+    )
+    .subscribe(
+      {
+        next: (res) => {
+          console.log('places:',res);
+          this.userPlaces.set(res)
+        },
+        error: (error:Error) => {
+          console.log("Error occured",error);
+          this.error.set(error.message)
+        },
+        complete: () => {
+          this.isFetchingData.set(false)
+        }
+      }
+    )
+  }
 }
