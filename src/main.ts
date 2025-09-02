cee90b9227853d07d418b8c1aff7cc4c9b62178a
robertgutu <gutu.robert49@yahoo.com>
2025-09-02 13:48:33 +0300

    how to add requeste inteceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClient, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+    
+    // const req = request.clone({
+    //     headers: request.headers.set('X-DEBUG', 'TESTING')
+    // })
+    console.log('outgoiong request!')
+    console.log(request);
+    // return next(req)
+    return next(request)
+}
 
 bootstrapApplication(AppComponent, {
-    providers:[provideHttpClient()]
+    providers:[provideHttpClient(
+        withInterceptors([loggingInterceptor])
+    )]
 }).catch((err) => console.error(err));
